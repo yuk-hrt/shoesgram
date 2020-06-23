888beac27c51a5f73e5471e5c11bbf7b250af580
yuk-hrt <62339023+yuk-hrt@users.noreply.github.com>
2020-06-23 12:26:55 +0900

    フォロー機能(非同期)の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    @name = current_user.name
-    @shoes = current_user.shoes
+    @user = User.find(params[:id])
+    @name = @user.name
+    @shoes = @user.shoes.order("created_at DESC")
+  end
+
+  def following
+    #@userがフォローしているユーザー
+    @user  = User.find(params[:id])
+    @users = @user.following
+  end
+
+  def followers
+    #@userをフォローしているユーザー
+    @user  = User.find(params[:id])
+    @users = @user.followers
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,32 @@ class User < ApplicationRecord
 
   has_many :shoes
   has_many :comments
-
+  
   has_many :favorites, dependent: :destroy
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
+  has_many :following, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
   # いいねを判断するメソッド
   def already_favorited?(shoe)
     self.favorites.exists?(shoe_id: shoe.id)
   end
+
+  
+  #フォローしているかを確認するメソッド
+  def following?(user)
+    self.following_relationships.find_by(following_id: user.id)
+  end
+
+  #フォローするときのメソッド
+  def follow(user)
+    self.following_relationships.create(following_id: user.id)
+  end
+
+  #フォローを外すときのメソッド
+  def unfollow(user)
+    self.following_relationships.find_by(following_id: user.id).destroy
+  end
+
 end
